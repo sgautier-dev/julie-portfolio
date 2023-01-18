b6d8eda693df2cdcf92a5d6b62d22f4a644fb1a2
sgautier-dev <sgautier.dev@gmail.com>
2023-01-18 11:42:40 +0400

    first images set and sticky header started

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,63 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import { groq } from "next-sanity";
+import { client } from "../lib/sanity.client";
+import urlFor from "../lib/urlFor";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-4xl" >Julie Gautier</h1>
-      <p>hello</p>
-    </div>
+// const query = groq`
+//   *[_type=='juliePics'] {
+//   ...
+// } | order(_createdAt desc)
+// `
+const query = groq`
+  *[_type=='juliePics'] {
+  image, alt, title
+}
+`;
 
-  )
+export default async function Home() {
+	const images: JuliePics[] = await client.fetch(query);
+	// console.log('images', images)
+	const firstImage = images.find((element) => element.title === "first")!;
+	console.log("first image", firstImage);
+	const secondImage = images.find((element) => element.title === "second")!;
+	console.log("second image", secondImage);
+  const thirdImage = images.find((element) => element.title === "third")!;
+	console.log("third image", thirdImage);
+
+	return (
+		<>
+			<header className="sticky top-0 z-0">
+				<h1 className="text-8xl lg:text-9xl">
+					Julie <br /> Gautier
+				</h1>
+			</header>
+
+			<div className="bg-orange-500 z-10">
+				<Image
+					className="z-10 w-2/3 lg:w-2/3 float-right mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+					src={urlFor(firstImage.image).url()}
+					alt={firstImage.alt}
+					width="2000"
+					height="1334"
+				/>
+				<Image
+					className="w-2/3 lg:w-2/3 float-right mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+					src={urlFor(secondImage.image).url()}
+					alt={secondImage.alt}
+					width="2000"
+					height="1334"
+				/>
+        <Image
+					className="w-2/3 lg:w-2/3 float-right mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+					src={urlFor(thirdImage.image).url()}
+					alt={thirdImage.alt}
+					width="4134"
+					height="2757"
+				/>
+			</div>
+		</>
+	);
 }
---
+++ typings.d.ts
@@ -0,0 +1,29 @@
+type Base = {
+    _createdAt: string
+    _id: string
+    _rev: string
+    _type: string
+    _updatedAt: string
+}
+
+interface Image {
+    _type: 'image'
+    asset: {
+        _ref: string
+        _type: 'reference'
+    }
+}
+
+interface JulieInfo extends Base {
+    _type: 'julieInfo'
+    title: string
+    image: Image
+    body: string
+}
+
+interface JuliePics extends Base {
+    _type: 'juliePics'
+    title: string
+    image: Image
+    alt: string
+}
