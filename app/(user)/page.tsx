52d7e19c6f673f7faeb0e8456021f9737122f4a5
sgautier-dev <sgautier.dev@gmail.com>
2023-01-23 17:24:12 +0400

    fonts added, bio and work started

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,57 +1,92 @@
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { Quicksand } from "@next/font/google";
 import { groq } from "next-sanity";
 import { client } from "../../lib/sanity.client";
 import urlFor from "../../lib/urlFor";
 
-const inter = Inter({ subsets: ["latin"] });
+const quicksand = Quicksand({
+	subsets: ["latin"],
+	// variable: '--font-title'
+});
 
 // const query = groq`
 //   *[_type=='juliePics'] {
 //   ...
 // } | order(_createdAt desc)
 // `
-const query = groq`
+const queryPics = groq`
   *[_type=='juliePics'] {
   image, alt, title
-}
-`;
+}`;
+
+const queryInfo = groq`
+  *[_type=='julieInfo'] {
+  image, body, title
+}`;
 
 export default async function Home() {
-	const images: JuliePics[] = await client.fetch(query);
+	const images: JuliePics[] = await client.fetch(queryPics);
 	// console.log('images', images)
+	const infos: JulieInfo[] = await client.fetch(queryInfo);
+	console.log("infos", infos);
 	const firstImage = images.find((element) => element.title === "first")!;
-	console.log("first image", firstImage);
 	const secondImage = images.find((element) => element.title === "second")!;
-	console.log("second image", secondImage);
 	const thirdImage = images.find((element) => element.title === "third")!;
-	console.log("third image", thirdImage);
+	// console.log("third image", thirdImage);
+	const bio = infos.find((element) => element.title === "Bio")!;
+	console.log("bio", bio);
+	const work = infos.find((element) => element.title === "Work")!;
+	console.log("work", work);
 
 	return (
 		<>
-			<main className="mx-auto z-10 border-solid border-2 border-red-600">
+			<main
+				className={`mx-auto border-solid border-2 border-red-600 ${quicksand.className}`}
+			>
 				<div className="flex flex-col place-items-end">
 					<Image
-						className="z-10 w-2/3 lg:w-2/3 mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+						className="images"
 						src={urlFor(firstImage.image).url()}
 						alt={firstImage.alt}
 						width="2000"
 						height="1334"
 					/>
 					<Image
-						className="z-10 w-2/3 lg:w-2/3 mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+						className="images"
 						src={urlFor(secondImage.image).url()}
 						alt={secondImage.alt}
 						width="2000"
 						height="1334"
 					/>
+
+					<div className="images border-solid border-3 border-yellow-600 flex flex-col gap-2 lg:flex-row">
+						<Image
+							className="z-10 w-3/4 lg:w-1/3 rounded-lg"
+							src={urlFor(bio.image).url()}
+							alt="julie gautier bio image"
+							width="2000"
+							height="2000"
+						/>
+						<p className="z-10 bg-slate-50">{bio.body}</p>
+					</div>
+
 					<Image
-						className="z-10 w-2/3 lg:w-2/3 mr-10 lg:mr-60 mb-20 lg:mb-30 rounded-lg"
+						className="images"
 						src={urlFor(thirdImage.image).url()}
 						alt={thirdImage.alt}
 						width="4134"
 						height="2757"
 					/>
+					<div className="images border-solid border-3 border-yellow-600 flex flex-col gap-2 lg:flex-row">
+						<Image
+							className="z-10 w-3/4 lg:w-1/3 rounded-lg"
+							src={urlFor(work.image).url()}
+							alt="julie gautier work image"
+							width="1334"
+							height="2000"
+						/>
+						<p className="z-10 bg-slate-50">{work.body}</p>
+					</div>
 				</div>
 			</main>
 		</>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,20 @@
+import { League_Spartan } from "@next/font/google";
+
+const leagueSpartan = League_Spartan({ 
+	subsets: ["latin"],
+	// variable: '--font-title' 
+});
+
 function Header() {
 	return (
-		<header className="sticky top-0 z-0 flex items-center justify-between space-x-2 px-3 py-3">
-			<div>
-				<h1 className="text-8xl lg:text-9xl">
-					Julie <br /> Gautier
+		<header className={`sticky top-0 z-0 flex items-center justify-between space-x-2 py-3 ${leagueSpartan.className}`}>
+			<div className="text-7xl md:text-8xl lg:text-9xl pl-2 md:pl-3 lg:pl-4">
+				<h1 >
+					JULIE <br /> GAUTIER
 				</h1>
 			</div>
-			<div>
-				<button className="px-4 py-2 bg-slate-900 text-slate-50 rounded-full text-center rotate-90">Menu</button>
+			<div className="rotate-90 text-2xl md:text-3xl lg:text-4xl border-b-2 border-slate-900 text-center hover:opacity-50 leading-none">
+				<button className="">menu</button>
 			</div>
 		</header>
 	);
