7d2f752430262053b96196e23eecf1b321e9e0d3
sgautier-dev <sgautier.dev@gmail.com>
2023-03-27 18:32:24 +0400

    meta title and description modified

--- app/(user)/head.tsx
+++ app/(user)/head.tsx
@@ -5,10 +5,10 @@ export default function Head() {
 			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta name="author" content="Sébastien Gautier" />
-			<title>Julie Gautier Underwater Author and Director </title>
+			<title>Julie Gautier - Water Tale</title>
 			<meta
 				name="description"
-				content="Julie Gautier's Portfolio, Underwater Author and Director"
+				content="Julie Gautier's Water Tale, Freediving Underwater Film Director, Creator of Aquaflow"
 			/>
 			<link rel="icon" href="/favicon.ico" />
 		</>
