39e13543c55dec1ae50bb3be827dbb130037a25b
sgautier-dev <sgautier.dev@gmail.com>
2024-02-02 11:02:35 +0400

    dependencies updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io', 'i.ytimg.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+    ],
   }
 }
 
